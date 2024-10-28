c4455ff9a3a7e87589ddda9bb81970089ff7f406
Sib <chengjie.michael.huang@gmail.com>
2024-10-28 00:40:43 -0700

    Linked search to App page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,55 @@
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import { useRouter } from "next/navigation";
+import { PlaceAutocomplete } from "~/components/autoComplete";
+const API_KEY: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
+console.log(API_KEY);
+
 export default function MainPage() {
+  const Router = useRouter();
+  const [selectedPlace, setSelectedPlace] =
+    useState<google.maps.places.PlaceResult | null>(null);
+
+  useEffect(() => {
+    if (selectedPlace) {
+      const lat = selectedPlace.geometry?.location.lat();
+      const lng = selectedPlace.geometry?.location.lng();
+      Router.push("/App" + "?lat=" + lat + "&lng=" + lng);
+    }
+  }, [selectedPlace, Router]);
+
+  // const [selectedPlace, setSelectedPlace] =
+  //   useState<google.maps.places.PlaceResult | null>(null);
+
+  // const Router = useRouter();
+
+  // useEffect(() => {
+  //   if (selectedPlace) {
+  //     // console.log(selectedPlace.geometry?.location.lat());
+  //     // console.log(selectedPlace.geometry?.location.lng());
+
+  //     Router.push("/App", {
+  //       query: {
+  //         lat: selectedPlace.geometry?.location.lat(),
+  //         lng: selectedPlace.geometry?.location.lng(),
+  //       },
+  //     });
+  //   }
+  // }, [selectedPlace]);
+
   return (
-    <div>
-      <h1>Header</h1>
-      <h1>Title</h1>
-      <h1>Description</h1>
-      <h1>Autocomplete Search Bar</h1>
+    <div className="flex-col">
+      <h1>Weekly Sunset Quality Forecast</h1>
+      <br></br>
+      <APIProvider
+        apiKey={API_KEY}
+        // solutionChannel="GMP_devsite_samples_v3_rgmautocomplete"
+      >
+        <div className="autocomplete-control">
+          <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+        </div>
+      </APIProvider>
     </div>
   );
 }
---
+++ src/components/autoComplete.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import { useMapsLibrary } from "@vis.gl/react-google-maps";
+
+interface PlaceAutocompleteProps {
+  onPlaceSelect: (place: google.maps.places.PlaceResult | null) => void;
+}
+
+export const PlaceAutocomplete = ({
+  onPlaceSelect,
+}: PlaceAutocompleteProps) => {
+  const [placeAutocomplete, setPlaceAutocomplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const places = useMapsLibrary("places");
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ["geometry"],
+      types: ["(cities)"],
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener("place_changed", () => {
+      onPlaceSelect(placeAutocomplete.getPlace());
+    });
+  }, [onPlaceSelect, placeAutocomplete]);
+
+  return (
+    <div className="autocomplete-container rounded-2xl border-2 border-black p-2">
+      <input ref={inputRef} />
+    </div>
+  );
+};
